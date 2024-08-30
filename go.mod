e0920b3b58223aca77bb701ae4ffbfacf8682584
mohammad <imousavi41@gmail.com>
2024-08-30 11:49:06 +0330

    Enhance database schema with improved constraints and indexing

--- go.mod
+++ go.mod
@@ -14,12 +14,16 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
@@ -34,6 +38,7 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
@@ -42,6 +47,7 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/go-playground/validator/v10 v10.22.0
 	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/google/uuid v1.5.0
 	github.com/jackc/pgx/v5 v5.6.0
--- internal/db/migration/000001_init_schema.up.sql
+++ internal/db/migration/000001_init_schema.up.sql
@@ -5,8 +5,8 @@ CREATE TABLE users (
     username VARCHAR(255) UNIQUE NOT NULL,
     hashed_password VARCHAR(255) NOT NULL,
     full_name VARCHAR(255) NOT NULL,
-    password_changed_at timestamptz NOT NULL DEFAULT('0001-01-01 00:00:00Z'),  
-    created_at timestamptz NOT NULL DEFAULT (now())
+    password_changed_at TIMESTAMPTZ NOT NULL DEFAULT('0001-01-01 00:00:00Z'),  
+    created_at TIMESTAMPTZ NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE cities (
@@ -16,27 +16,27 @@ CREATE TABLE cities (
 
 CREATE TABLE terminals (
     id SERIAL PRIMARY KEY,
-    city_id INT REFERENCES cities(id),
+    city_id INT NOT NULL REFERENCES cities(id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     UNIQUE(city_id, name)
 );
 
 CREATE TABLE routes (
     id SERIAL PRIMARY KEY,
-    origin_terminal_id INT REFERENCES terminals(id),
-    destination_terminal_id INT REFERENCES terminals(id),
+    origin_terminal_id INT NOT NULL REFERENCES terminals(id) ON DELETE CASCADE,
+    destination_terminal_id INT NOT NULL REFERENCES terminals(id) ON DELETE CASCADE,
     duration INTERVAL NOT NULL,
-    distance INT NOT NULL,
+    distance INT NOT NULL CHECK (distance > 0),
     UNIQUE(origin_terminal_id, destination_terminal_id)
 );
 
 CREATE TABLE buses (
     id SERIAL PRIMARY KEY,
-    route_id INT REFERENCES routes(id),
+    route_id INT NOT NULL REFERENCES routes(id) ON DELETE CASCADE,
     departure_time TIMESTAMPTZ NOT NULL,
     arrival_time TIMESTAMPTZ NOT NULL,
-    capacity INT NOT NULL,
-    price INT NOT NULL,
+    capacity INT NOT NULL CHECK (capacity > 0),
+    price INT NOT NULL CHECK (price > 0),
     bus_type VARCHAR(255) NOT NULL,
     corporation VARCHAR(255),
     super_corporation VARCHAR(255),
@@ -46,39 +46,44 @@ CREATE TABLE buses (
 
 CREATE TABLE bus_seats (
     id SERIAL PRIMARY KEY,
-    bus_id INT REFERENCES buses(id),
-    seat_number INT NOT NULL,
+    bus_id INT NOT NULL REFERENCES buses(id) ON DELETE CASCADE,
+    seat_number INT NOT NULL CHECK (seat_number > 0),
     status INT NOT NULL,
     passenger_national_code VARCHAR(255)
 );
 
 CREATE TABLE tickets (
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
-    bus_id INT REFERENCES buses(id),
-    seat_id INT REFERENCES bus_seats(id),
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    bus_id INT NOT NULL REFERENCES buses(id) ON DELETE CASCADE,
+    seat_id INT NOT NULL REFERENCES bus_seats(id) ON DELETE CASCADE,
     reserved_at TIMESTAMPTZ DEFAULT NOW(),
     UNIQUE(user_id, bus_id, seat_id)
 );
 
 CREATE TABLE penalties (
     id SERIAL PRIMARY KEY,
-    bus_id INT REFERENCES buses(id),
-    actual_hours_before FLOAT,
-    hours_before FLOAT,
-    percent INT NOT NULL,
+    bus_id INT NOT NULL REFERENCES buses(id) ON DELETE CASCADE,
+    actual_hours_before FLOAT CHECK (actual_hours_before >= 0),
+    hours_before FLOAT CHECK (hours_before >= 0),
+    percent INT NOT NULL CHECK (percent >= 0 AND percent <= 100),
     custom_text TEXT
 );
 
-CREATE TABLE "sessions" (
-  "id" uuid PRIMARY KEY,
-  "username" varchar NOT NULL,
-  "refresh_token" varchar NOT NULL,
-  "user_agent" varchar NOT NULL,
-  "client_ip" varchar NOT NULL,
-  "is_blocked" boolean NOT NULL DEFAULT false,
-  "expires_at" timestamptz NOT NULL,
-  "created_at" timestamptz NOT NULL DEFAULT (now())
+CREATE TABLE sessions (
+  id UUID PRIMARY KEY,
+  username VARCHAR NOT NULL,
+  refresh_token VARCHAR NOT NULL,
+  user_agent VARCHAR NOT NULL,
+  client_ip VARCHAR NOT NULL,
+  is_blocked BOOLEAN NOT NULL DEFAULT false,
+  expires_at TIMESTAMPTZ NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT (now()),
+  FOREIGN KEY (username) REFERENCES users (username) ON DELETE CASCADE
 );
 
-ALTER TABLE "sessions" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
+CREATE INDEX idx_routes_origin_destination ON routes (origin_terminal_id, destination_terminal_id);
+CREATE INDEX idx_buses_departure_time ON buses (departure_time);
+CREATE INDEX idx_bus_seats_status ON bus_seats (status);
+CREATE INDEX idx_tickets_reserved_at ON tickets (reserved_at);
+CREATE INDEX idx_sessions_username ON sessions (username);
